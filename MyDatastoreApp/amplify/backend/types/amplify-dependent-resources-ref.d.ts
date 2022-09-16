1d6a576457f42b2d6903e4265888a88d8d260e56
poojamat <poojamat@gmail.com>
2022-09-16 11:58:38 -0700

    Delete MyDatastoreApp directory

--- MyDatastoreApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "mydatastoreapp": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
